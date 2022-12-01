355033dcc84d308837f6d57d71c6a38684ea18b7
shidayang <530847445@qq.com>
2022-12-01 14:54:28 +0800

    fix presto read hudi bug about timestamp type

--- docker/benchmark/build-image.sh
+++ docker/benchmark/build-image.sh
@@ -80,7 +80,7 @@ ${BUILD_CMD} \
 ${BUILD_CMD} \
   --build-arg BENCHMARK_VERSION=${BENCHMARK_VERSION} \
   --file "${SELF_DIR}/images/benchmark-lakehouse-ingestion.Dockerfile" \
-  --tag arctic163/benchmark-lakehouse-ingestion:${BENCHMARK_VERSION} \
+  --tag arctic163/lakehouse-benchmark-ingestion:${BENCHMARK_VERSION} \
   "${SELF_DIR}/images" $@
 
 ${BUILD_CMD} \
